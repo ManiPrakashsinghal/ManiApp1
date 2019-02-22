7019c3f55ceaea3896bab955f06e9f0b8caab429
t120 <t120@t120.sagdomain.local>
2019-02-22 16:23:02 +0530

    cmnt

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,3 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+/**
+ * 
+ * remove you current version : npm uninstall @agm/core
+then install this version: npm i @agm/core@1.0.0-beta.3 --save
+ * 
+ * 
+ */
